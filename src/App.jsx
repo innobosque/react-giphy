3d7dac37d2a17c52f61f84519d0012c16fabd44d
webferrol <xurxo@webferrol.com>
2022-10-16 00:59:06 +0200

    Puesto color de fondo al header

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
     }
     return (
         <>
-            <header className={`p-4 bg-[url(./assets/bg-mobile-light.jpg)] bg-no-repeat bg-left-top bg-cover`}>
+            <header className={`p-4 bg-blue-400 bg-[url(./assets/bg-mobile-light.jpg)] bg-no-repeat bg-left-top bg-cover`}>
                 <div className="flex justify-between">
                     <h1 className="uppercase text-white text-3xl font-bold space-x-7 tracking-wider">App Gifphy</h1>
                     <button className="bg-no-repeat bg-contain bg-[url(./assets/icon-moon.svg)]">&nbsp;&nbsp;&nbsp;&nbsp;</button>
