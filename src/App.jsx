595eaa9317106ec0919382e71f44f77b374e6402
webferrol <xurxo@webferrol.com>
2022-10-16 16:52:22 +0200

    Instalado plugin tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
         <>
             <header className={`p-4 bg-blue-400 bg-[url('./assets/bg-mobile-light.jpg')] bg-no-repeat bg-left-top bg-cover`}>
                 <div className="flex justify-between">
-                    <h1 className="uppercase text-white text-3xl font-bold space-x-7 tracking-wider">App Gifphy</h1>
+                    <h1 className="uppercase text-white  font-bold space-x-7 tracking-wider">App Gifphy</h1>
                     <button className="bg-no-repeat bg-contain bg-[url('./assets/icon-moon.svg')]">&nbsp;&nbsp;&nbsp;&nbsp;</button>
                 </div>
                 <AddGif onNewGif={value => handleNewGif(value)} />
